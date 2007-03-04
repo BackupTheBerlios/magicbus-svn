0acbd97279f180742d69648878adc2af94614beb
cazenave <cazenave@6c39f0b7-872a-0410-82a0-b259cab2f439>
2007-03-04 17:31:04 +0000

    commit parce qu'on se fait chier lol avec un test du driver!!!

--- trunk/src/Main.adb
+++ trunk/src/Main.adb
@@ -1,4 +1,4 @@
-with Interfaces.C,Text_io,BusStop_package,common_types_ptr,common_types;use Text_io,BusStop_package,common_types_ptr,Interfaces.C,common_types;
+with Interfaces.C,Text_io,BusStop_package,bus_package,common_types_ptr,common_types;use Text_io,BusStop_package,common_types_ptr,Interfaces.C,bus_package,common_types;
 
 
 procedure Main is 
@@ -6,19 +6,18 @@ procedure Main is
     Seconde : constant duration := 1.0;
     pos1 : ptrT_position:= new T_position'(3, 5);
     pt : ptrString:=new String'("Arret 1");
-    B1 : BusStop_package.BusStop(1,pt,pos1);
+    A1 : BusStop_package.BusStop(1,pt,pos1);
     pos2 : ptrT_position:= new T_position'(5, 15);
     pt2: ptrString :=new String'("Arret 2");
-    B2 : BusStop_package.BusStop(2,pt2,pos2); 
+    A2 : BusStop_package.BusStop(2,pt2,pos2); 
     Tab: array (0..5) of T_Arret;
+    B1:Driver;
 begin
     put_line("gogogogogo");
     delay(5*Seconde);
-    
-    B1.receiveDisplay("Affichage sur l'ecran de l'arret 1");
-    B1.emit(pos1.all);
-    B2.emit(pos1.all);
-    
-    
-    --put_line(B1.name);
+    B1.calculateSpeed(5.0);
+    A1.receiveDisplay("Affichage sur l'ecran de l'arret 1");
+    A1.emit(pos1.all);
+    A2.emit(pos1.all);
+    B1.calculateSpeed(-70.0);
 end Main;
