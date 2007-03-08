a29acd12d7caa58ba8f3cbe846e563aa054e9282
cazenave <cazenave@6c39f0b7-872a-0410-82a0-b259cab2f439>
2007-03-08 10:42:53 +0000

    main.c qui va bien le traitement de lancement ada est dans un thread
    FLO FLO I LOVE YOU

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 
-extern void lancement();
+extern void lancement(char * s);
 extern void adainit();
 extern void adafinal();
 void affichage(char * test[]);
 
+
 int main(int argc, char *argv[]){
-        printf("%s\n","Main C en route");
-        adainit();
-        lancement("Bienvenue dans magicbus (param envoye de C vers ADA)");
-        adafinal();
-        return 0;
+    pthread_t id_thread;
+    char * s="Bienvenue dans magicbus (param envoye de C vers ADA)";
+    int res;
+    printf("%s\n","Main C en route");
+    adainit();
+    res=pthread_create(&id_thread,NULL,(void *) lancement,s);
+    if (res!=0)
+    {
+       printf("error");
+       exit(2);
+    }
+    pthread_join(id_thread,NULL);
+    adafinal();
+    return 0;
 }
 
 void affichage(char * test[])
